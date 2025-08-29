999474c6d5ee68bb27c61e1e6d3e0339fc8d9d1c
Muhammad Haris <muhammadhxris01@gmail.com>
2025-08-29 18:15:56 +0100

    remove readme

---
+++ Shopping_List_Manager.py
@@ -0,0 +1,31 @@
+print("Shopping List: \n")
+print("Commands \n \n Add [x] - adds item \n Remove [x] - removes item \n Show - Shows list \n Clear - Clears list \n Quit - Ends program \n")
+shopping_list=[]
+program = True
+
+while program==True:
+    command=input("Type in your command: \n").lower()
+    if "add" in command:
+        new_command=command.replace("add","").strip()
+        shopping_list.append(new_command)
+    elif "remove" in command:
+        new_command=command.replace("remove","").strip()
+        if new_command in shopping_list:
+                 shopping_list.remove(new_command)
+        else:
+            print("This item doesn't exist. Please try again \n")
+    elif "show" in command:
+        for i in shopping_list:
+            print(f"\n -{i}\n")
+    elif "clear" in command:
+        shopping_list.clear()
+    elif "quit" in command:
+        program=False
+    else:
+        print("Incorrect Input. Please try again")
+
+print("\nHere is your shopping list:\n")
+for i in shopping_list:
+    print(f" - {i} \n")
+
+    
